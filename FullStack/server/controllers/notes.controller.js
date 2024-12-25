03546c0fcf7f433a94825ca92907790e5661fb44
Divyesh-Thakur45 <divyeshthakur370@gmail.com>
2024-12-25 12:07:44 +0530

    update

--- FullStack/server/controllers/notes.controller.js
+++ FullStack/server/controllers/notes.controller.js
@@ -15,4 +15,20 @@ const NotesCreate = async (req, res) => {
     res.status(500).send({ message: "Server error" });
   }
 };
-module.exports = { NotesCreate };
+
+const NotesDelete = async (req, res) => {
+  const { id } = req.params;
+  const isDelete = await NotesModel.findById(id);
+  // res.send(isDelete.userId)
+  if (!isDelete) {
+    return res.status(404).send({ message: "Note not found" });
+  }
+  if (isDelete.userId != req.user._id) {
+    return res
+      .status(403)
+      .send({ message: "You are not authorized to delete this note" });
+  }
+  await NotesModel.findByIdAndDelete(isDelete);
+  return res.status(200).send({ message: "Note deleted successfully" });
+};
+module.exports = { NotesCreate, NotesDelete };
